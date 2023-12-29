585c2326de2b26b8a3fb844403c2c37d1a61d8ad
Kai D. Gonzalez <gkai70263@gmail.com>
2023-12-29 12:51:01 -0500

    add a classic test 3

---
+++ SIBC/examples/test3-stack-reg-classic.asm
@@ -0,0 +1,17 @@
+; $Id: test3-stack-reg-classic.asm
+
+; a simple test for stack and registers
+main:
+    mov R1,5
+    mov R2,5
+
+    cmp R1,R2,a,b
+
+a:
+    echo 'A'
+    echo 'B'
+    echo 'C'
+b:
+    echo 'D'
+    echo 'E'
+    echo 'F'
