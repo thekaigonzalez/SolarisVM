579fcf7b0d756ae252dfbabd042fb6e5921c9d51
Kai D. Gonzalez <gkai70263@gmail.com>
2023-12-29 13:25:15 -0500

    add gitignore and also fix example

--- SIBC/examples/test3-stack-reg-classic.asm
+++ SIBC/examples/test3-stack-reg-classic.asm
@@ -1,12 +1,5 @@
 ; $Id: test3-stack-reg-classic.asm
 
-; a simple test for stack and registers
-main:
-    mov R1,5
-    mov R2,5
-
-    cmp R1,R2,a,b
-
 a:
     echo 'A'
     echo 'B'
@@ -15,3 +8,10 @@ b:
     echo 'D'
     echo 'E'
     echo 'F'
+
+; a simple test for stack and registers
+m:
+    mov R1,5
+    mov R2,5
+
+    cmp R1,R2,a,b
